9775ed0f0eb09a36457ac184f03b987cfb2f915f
John Zastrow <br8kwall@gmail.com>
2018-04-13 18:54:04 -0400

    Comment out infinite loop

--- dicts.py
+++ dicts.py
@@ -27,3 +27,26 @@ while countb < 10:
          continue
      print(f"We're counting odd numbers: {countb}")
      countb += 1
+
+initial = 0
+while initial < 100:
+    if initial % 2 == 0:
+        initial += 1
+        continue
+    elif initial > 90:
+        print("Hey, I hit 90 before reaching 100, I'll stop here")
+        break
+    else:
+        print(f"Let's do a lot of numbers like this: {initial}")
+    initial += 1
+
+# comb = 1
+# while comb <= 1000:
+#    print({comb})
+#    comb += 1
+#    if comb > 900:
+#        print("Wow, I'm tired")
+#        break
+#    else:
+#        print(f"Boom: {comb}")
+#    comb += 1
