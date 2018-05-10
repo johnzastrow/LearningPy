75ecfd931c07f7a8bf0e0bf9cd96922bc60b41e6
John Zastrow <br8kwall@gmail.com>
2018-05-10 12:34:51 -0400

    nothing

--- dicts.py
+++ dicts.py
@@ -1,15 +1,41 @@
 # I am some dictionaries
-ages = { 'Kevin' : 59, 'alex': 29, 'bob': 40}
+ages = {'Kevin': 59, 'alex': 29, 'bob': 40}
 ages
-print (ages)
+print(ages)
+
+for i in ages:
+    print('i')
+    print("888888888888")
+
+for noun in ['dog', 'cat', 'poodle', 'Maine Coon cat']:
+    print(noun)
+    print("-----------------------")
+
+for me in [1, 2, 3, 4, 5, 6]:
+    print(me)
+    print('me------')
+
+print("*********************************************")
+for element_from_pets in ['doggy', 'catty', 'poodlely', 'Maine Coon catty']:
+    print(element_from_pets)
+    print("")
+
+
+pets = ['dog', 'cat', 'poodle', 'Maine Coon cat']
+total = 0
+for pet in pets:
+    length = len(pet)
+    total = total + length
+    print(length)
+print(f"And now the total: {total}")
 
 name = "Kevin"
 if len(name) >= 6:
-     print("name is long")
+    print("name is long")
 elif len(name) == 5:
-     print("name is 5 characters")
+    print("name is 5 characters")
 elif len(name) >= 4:
-# will stop above becuase its true
+    # will stop above becuase its true
     print("name is 4 or more")
 else:
     print("name is short")
@@ -22,11 +48,12 @@ while count <= 4:
 
 countb = 0
 while countb < 10:
-     if countb % 2 == 0:
-         countb += 1
-         continue
-     print(f"We're counting odd numbers: {countb}")
-     countb += 1
+    if countb % 2 == 0:
+        countb += 1
+        continue
+    print(f"We're counting odd numbers: {countb}")
+    countb += 1
+    print("Demoing Git")
 
 initial = 0
 while initial < 100:
